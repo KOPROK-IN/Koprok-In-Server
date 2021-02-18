f62723ba0a04b40d60466e3ab29e7bc353b3ff9a
Mukti Aji Guno <ajioke22@gmail.com>
2021-02-18 22:07:58 +0700

    Add Data Seed

--- seeders/20210218142847-add-data.js
+++ seeders/20210218142847-add-data.js
@@ -1,10 +1,11 @@
 'use strict';
+const { hashPass } = require('../helpers/bcrypt')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
       email: 'example1@mail.com',
-      password: '123456',
+      password: hashPass('123456'),
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
