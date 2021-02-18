0c1edafc08018a60b0dae9132b80b8c1d41e66bd
Mukti Aji Guno <ajioke22@gmail.com>
2021-02-18 21:48:35 +0700

    Add cors and seeding

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test'){
 const express = require('express')
 const route = require('./routes/index')
 const app = express()
+const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
