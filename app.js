ece82eeeddc1df71ced1c0934e14ace3560a6e0d
milandak19 <fiqrimilano@gmail.com>
2021-02-18 21:52:07 +0700

    config socket.io

--- app.js
+++ app.js
@@ -6,14 +6,29 @@ const express = require('express')
 const route = require('./routes/index')
 const app = express()
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors')
 const port = process.env.PORT || 3000
+const http = require('http').Server(app)
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+  allowEIO3: true
+})
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', route)
 app.use(errorHandler)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(port, () => {
   console.log('app is running on port :', port)
 })
